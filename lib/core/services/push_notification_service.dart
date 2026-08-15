157ebc8cd2de0c60a95edd0f3be57d0db053d1f5
NikhilKhatri <khatrinikhil431@gmail.com>
2026-08-15 21:20:14 +0545

    notification

--- lib/core/services/push_notification_service.dart
+++ lib/core/services/push_notification_service.dart
@@ -5,7 +5,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../routing/app_router.dart';
+import 'package:pyago/core/routing/app_router.dart';
 
 /// Push notification integration point.
 ///
